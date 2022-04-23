8775b4dc010195d1ec9966fcfed626148d5198e9
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-04-23 23:04:06 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require("cors");
+require("dotenv").config()
+const app = express()
+
+const port = process.env.PORT || 5000;
+
+
+//use middleware
+app.use(cors());
+app.use(express.json())
+
+//
+app.get('/', (req, res)=>{
+    res.send("Running Genuis server")
+})
+
+app.listen(port, ()=>{
+    console.log("Server is running");
+})
