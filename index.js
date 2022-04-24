0b9d1ceb10986651763df4b77333036b22610230
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-04-25 03:47:17 +0600

    mongoDB connect

--- index.js
+++ index.js
@@ -10,9 +10,26 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json())
 
+
+//monir
+//OenJ3isYvCDwUcrE
+
+//MongoDB
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://monir:OenJ3isYvCDwUcrE@geniuscarusers.jkay7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log("MongoDB is Connected");
+//   client.close();
+});
+
+
 //
 app.get('/', (req, res)=>{
-    res.send("Running Genuis server")
+    res.send(users)
 })
 
 app.listen(port, ()=>{
