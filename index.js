845234123f1c04d154f65c2d0f0f42ca86229eae
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-04-25 11:00:52 +0600

    Practice

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require("cors");
+const ObjectId = require("mongodb").ObjectId;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require("dotenv").config()
 const app = express()
 
@@ -15,22 +17,46 @@ app.use(express.json())
 //OenJ3isYvCDwUcrE
 
 //MongoDB
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://monir:OenJ3isYvCDwUcrE@geniuscarusers.jkay7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  console.log("MongoDB is Connected");
-//   client.close();
-});
 
+async function run() {
+  try{
+    await client.connect();
+    const userCollection = client.db("GeniusCar").collection("user");
 
-//
-app.get('/', (req, res)=>{
-    res.send(users)
-})
+    //POST API
+    app.post('/user', async(req, res)=>{
+      const newUser = req.body;
+      res.send(newUser);
+      const result = await userCollection.insertOne(newUser)
+
+    })
+
+    //GET API
+    app.get('/users', async (req, res)=>{
+      const query = {};
+      const cursor = userCollection.find(query);
+      const users = await cursor.toArray();
+      res.send(users)
+    })
+
+    //Delete API
+    app.delete('/user/:id', async (req,res)=>{
+      const id =req.params.id;
+      const query = {_id: ObjectId(id)};
+      const result = await userCollection.deleteOne(query);
+      res.send(result)
+    })
+
+  }
+  finally{
+
+  }
+
+}
+run().catch(console.dir)
+
 
 app.listen(port, ()=>{
     console.log("Server is running");
